7bc0ac0f3709894fa2527249f256a71b852ad825
jpb <jakub_baranski@outlook.com>
2025-12-30 06:55:21 +0000

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,11 +91,11 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.13.0")
+    implementation("com.google.android.material:material:1.14.0-alpha08")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.17.0")
 
-    val cameraVersion = "1.6.0-alpha01"
+    val cameraVersion = "1.6.0-alpha02"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
@@ -103,5 +103,5 @@ dependencies {
     implementation("androidx.camera:camera-view:$cameraVersion")
     implementation("androidx.camera:camera-extensions:$cameraVersion")
 
-    implementation("com.google.zxing:core:3.5.3")
+    implementation("com.google.zxing:core:3.5.4")
 }
